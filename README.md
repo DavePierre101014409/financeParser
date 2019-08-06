7758f1f54af594c41574d2213436d8503ddfe650
Dave Pierre <davepierre@cmail.carleton.ca>
2019-08-05 20:19:38 -0400

    added database and folders

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # financeParser
+
+This project is to take a pdf from scotiabank and able to parse the name the price and the date of each transcaction and write it to a database. 
+
+
+
+Future Ideas:
+
+display some graphs
+Use data analytics
+use Angular to display information
+
+
+
--- database.py
+++ database.py
@@ -7,7 +7,7 @@ db = sqlite3.connect(nameOfDatabase)
 db.execute("CREATE TABLE IF NOT EXISTS monthlyTransCation (name TEXT, price INTEGER, date TEXT)")
 db.execute("INSERT INTO monthlyTransCation(name,price, date) VALUES('Walmart',12.99,'2018-08-01')")
 
-
+#
 cursor = db.cursor()
 cursor.execute("SELECT * FROM monthlyTransCation")
 
@@ -17,4 +17,8 @@ for name ,price , date in cursor:
     print(date)
 
     print("-"*20)
+
+cursor.close()
+cursor.connection.commit()
+
 db.close()
