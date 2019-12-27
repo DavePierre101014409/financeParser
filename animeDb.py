1f12778c33f155f82aa0f7dfab26f2630096fff6
Dave Pierre <35567910+DavePierre101014409@users.noreply.github.com>
2019-12-27 09:40:45 -0500

    Create animeDb.py
    added a insertd anime command

---
+++ animeDb.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+#https://likegeeks.com/python-sqlite3-tutorial/
+
+nameOfDatabase = "animeTracker.sqlite"
+db = sqlite3.connect(nameOfDatabase)
+db.execute("CREATE TABLE IF NOT EXISTS anime(id INTEGER PRIMARY KEY, eng_title TEXT,jap_title TEXT, inserted_date TEXT)")
+
+#
+cursor = db.cursor()
+cursor.execute("SELECT * FROM monthlyTransCation")
+
+for name ,price , date in cursor:
+    print(name)
+    print(price)
+    print(date)
+
+    print("-"*20)
+
+cursor.close()
+cursor.connection.commit()
+
+db.close()
+
+def create_connection(db_name):
+    try:
+        con = sqlite3.connect(db_name)
+        return con
+
+    execept Error as e:
+        print(e)
+
+    return None
+
+def insertNewAnime(eng_title, jap_title, inserted_Date):
+    con =create_connection(nameOfDatabase).cursor()
+    values= (eng_title, jap_title, inserted_Date)
+    con.execute('INSERT INTO anime(id, eng_title, jap_title ,inserted_date) VALUES(?,?,?,?)', values)
+    con.commit()
